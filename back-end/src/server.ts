1241297aaf64a2565510de505980e0929d3e327d
Odair Cesar Ferreira <odairferreira97@gmail.com>
2023-05-16 23:31:10 -0300

    Criando CRUD para as Memories

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
 const prisma = new PrismaClient()
 
 app.get('/users', async () => {
